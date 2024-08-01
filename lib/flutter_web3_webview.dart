751ae20accd24d7b357c852fcc81080398a9e053
peipeiTu <37138187+peipeiTu@users.noreply.github.com>
2024-08-01 09:51:45 +0800

    update: Export js callback data.

--- lib/flutter_web3_webview.dart
+++ lib/flutter_web3_webview.dart
@@ -2,4 +2,6 @@ library flutter_web3_webview;
 
 export 'package:flutter_inappwebview/flutter_inappwebview.dart';
 export 'package:flutter_web3_webview/src/models/settings.dart';
+
+export 'package:flutter_web3_webview/src/models/js_callback_data.dart';
 export 'package:flutter_web3_webview/src/webview.dart';
